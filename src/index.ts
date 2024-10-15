6f589ff6173f8c3758f3db24666849e2ab15e19c
alejandrovarelamr <alejandro.varela@meru.com>
2024-10-15 16:26:24 -0500

    chore: Adding logging

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,20 @@
 import * as core from '@actions/core';
 import madge from 'madge';
-import { exec } from 'child_process';
 
 async function run() {
   try {
     // Get inputs from the action
     const modifiedFilesInput = core.getInput('modified_files');
     const pagesToCheckInput = core.getInput('pages_to_check');
-    const projectDirectory = core.getInput('project_directory') || './';
-    const madgeOptions = core.getInput('madge_options') || '';
+    const host_output = core.getInput('host_output') || 'http://localhost:3000/';
 
     const modifiedFiles = modifiedFilesInput.split(' ');
     const pagesToCheck = pagesToCheckInput.split(',');
-
-    //logs
-    console.log('modifiedFilesInput:', modifiedFilesInput);
-    console.log('pagesToCheckInput:', pagesToCheckInput);
-    console.log('projectDirectory:', projectDirectory);
-    console.log('madgeOptions:', madgeOptions);
-    console.log('modifiedFiles:', modifiedFiles);
-    console.log('pagesToCheck:', pagesToCheck);
 
     // Filter to include only files that start with 'src' and remove the 'src/' prefix
     const filteredModifiedFiles = modifiedFiles
       .filter(file => file.startsWith('src'))
       .map(file => file.replace(/^src\//, '')); // Remove 'src/' prefix
-    console.log('filteredModifiedFiles:', filteredModifiedFiles);
-
-    // ls -a in current directory
-    exec('ls -a', (err: any, stdout: any, stderr: any) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(stdout);
-    });
 
     const affectedPages: string[] = [];
 
@@ -45,8 +25,6 @@ async function run() {
 
       // Get the list of dependencies for the current page
       const dependencies = dependencyTree.obj();
-
-      console.log('dependencies:', dependencies);
 
       // Check if any modified file is a dependency of the current page
       for (const modifiedFile of filteredModifiedFiles) {
@@ -65,8 +43,18 @@ async function run() {
     }
 
     console.log('affectedPages:', affectedPages);
+
+    const formatOutput = affectedPages.map(page =>
+      page.replace(/^src\/pages\//, '').replace(/^src\/app\//, '').replace(/\.tsx$/, '')
+        .replace(/\/index$/, '').replace(/\/page$/, '').replace(/\/\([^)]*\)/g, '')
+    )
+      .map(page => `${host_output}${page}`)
+      .join('\n');
+
+    console.log('formatOutput:', formatOutput);
+
     // Output the affected pages as a comma-separated list
-    core.setOutput('affected_pages', affectedPages.length > 0 ? affectedPages.join(',') : '[]');
+    core.setOutput('affected_pages', affectedPages.length > 0 ? formatOutput : '');
 
   } catch (error) {
     core.setFailed(`Action failed with error: ${error}`);
